bef72fbb5197db5bc6df9a99b20f961dbd303452
chiyu-git <873705939@qq.com>
2019-05-31 21:16:41 +0800

    add flux pubsub etc.

---
+++ src/lib/pubsub/index.js
@@ -0,0 +1,43 @@
+export default class PubSub {
+  constructor() {
+    this.eventList = {};
+  }
+  /**
+   * @param  {String} event 
+   * @param  {Function} handler 
+   *
+   * @api private
+   */
+  subscribe(event, handler) {
+    if(!this.eventList.hasOwnProperty(event)) {
+      this.eventList[event] = [];
+    }
+    return this.eventList[event].push(handler);
+  }
+  /**
+   * @param  {String} event 
+   *
+   * @api private
+   */
+  publish(event,...rest) {
+    if(!this.eventList.hasOwnProperty(event)){
+      console.log('事件未定义');
+      return
+    }
+    
+    this.eventList[event].forEach(handler => handler(...rest));
+  }
+  /**
+   * @param  {String} event 
+   * @param  {Function} handler 
+   * @api private
+   */
+  unsubscribe(event,handler){
+    const handlers = this.eventList[event]
+    if(handlers===undefined) return
+
+    const handlerIndex = handlers.indexOf(handler)
+    handlers.splice(handlerIndex,1)
+    console.log('成功解除事件绑定');
+  }
+}
